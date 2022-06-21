fff171cbddcb404bcd1be26f1afd3a6c80883372
FickleBobby <chutney@Stevens-iMac.local>
2022-06-21 13:35:08 +0100

    Update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+
+	projectId: "f67wwz",
 	e2e: {
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
 		baseUrl: "http://localhost:4200/",
+
 	},
 });
